68105b3aa093666d5a5a33539aa3733eef257600
zeguilherme99 <zeguilherme008@hotmail.com>
2021-07-02 21:01:12 -0300

    Corrigido o bug da mensagem motivacional que não aparecia.

--- src/TeacherHelper.java
+++ src/TeacherHelper.java
@@ -57,6 +57,9 @@ public class TeacherHelper {
 			case 3:
 				
 				switch(gerador.nextInt(7)) {
+				case 0: 
+					System.out.println("Mesmo de sexta-feira, você está no gás.");
+					break;
 				case 1:
 					System.out.println("Você é um professor incrível!");
 					break;
